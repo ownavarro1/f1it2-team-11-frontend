eb832be06fd48d2217dee8cd31029dbeb15cc4b4
ownavarro1 <112905824+ownavarro1@users.noreply.github.com>
2023-06-24 10:28:41 -0500

    Commit 1

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CoursesComponent } from './courses/courses.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeadersComponent,
+    FooterComponent,
+    SearchComponent,
+    CoursesComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
