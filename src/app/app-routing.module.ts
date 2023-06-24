5d21bcad646af68a35500d8d03e3e4e2157ac7c3
ownavarro1 <112905824+ownavarro1@users.noreply.github.com>
2023-06-24 17:05:34 -0500

    commit-oscar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule, importProvidersFrom } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilEstudianteComponent } from './perfil.estudiante/perfil.estudiante.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 
 const routes: Routes = [
   {path :'perfil.estudiante',component:PerfilEstudianteComponent },
   {path:'estudiante',loadChildren:()=>import('./estudiante/estudiante.module').then(val => val.estudianteModule)},
-  {path:'**',pathMatch:'full',redirectTo:'perfil-estudiante'}
+  {path:'**',pathMatch:'full',redirectTo:'perfil-estudiante'},
+  {path: "login", component:LoginComponent},
+  { path: "register",component:RegisterComponent},
+  {path:"forgot-password", component:ForgotPasswordComponent}
 ]; 
 
 @NgModule({
